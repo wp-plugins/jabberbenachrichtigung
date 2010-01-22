673179255a834e6c573e63510011c07522da1130
missi <missi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-22 19:33:14 +0000

    gettext
    git-svn-id: https://plugins.svn.wordpress.org/jabberbenachrichtigung/trunk@196829 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 3.141592/jabbernotification.php
+++ 3.141592/jabbernotification.php
@@ -166,38 +166,38 @@ function jabbernotification($comment_id = 0)
 			switch ($comment->comment_type)
 			{
 		       	 	case 'trackback':
-		       	        	$notify_message  = sprintf( __('A new trackback on the post #%1$s "%2$s" is waiting for your approval'), $post->ID, $post->post_title ) . "\r\n";
+		       	        	$notify_message  = sprintf( __('A new trackback on the post #%1$s "%2$s" is waiting for your approval', 'jabbernotification'), $post->ID, $post->post_title ) . "\r\n";
 		       	        	$notify_message .= get_permalink($comment->comment_post_ID) . "\r\n\r\n";
-		        	        $notify_message .= sprintf( __('Website : %1$s (IP: %2$s , %3$s)'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
-		                	$notify_message .= sprintf( __('URL    : %s'), $comment->comment_author_url ) . "\r\n";
-		                	$notify_message .= __('Trackback excerpt: ') . "\r\n" . $comment->comment_content . "\r\n\r\n";
+		        	        $notify_message .= sprintf( __('Website : %1$s (IP: %2$s , %3$s)', 'jabbernotification'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
+		                	$notify_message .= sprintf( __('URL    : %s', 'jabbernotification'), $comment->comment_author_url ) . "\r\n";
+		                	$notify_message .= __('Trackback excerpt: ', 'jabbernotification') . "\r\n" . $comment->comment_content . "\r\n\r\n";
 		                	break;
 		        	case 'pingback':
-		                	$notify_message  = sprintf( __('A new pingback on the post #%1$s "%2$s" is waiting for your approval'), $post->ID, $post->post_title ) . "\r\n";
+		                	$notify_message  = sprintf( __('A new pingback on the post #%1$s "%2$s" is waiting for your approval', 'jabbernotification'), $post->ID, $post->post_title ) . "\r\n";
 		                	$notify_message .= get_permalink($comment->comment_post_ID) . "\r\n\r\n";
-		                	$notify_message .= sprintf( __('Website : %1$s (IP: %2$s , %3$s)'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
-		                	$notify_message .= sprintf( __('URL    : %s'), $comment->comment_author_url ) . "\r\n";
-		                	$notify_message .= __('Pingback excerpt: ') . "\r\n" . $comment->comment_content . "\r\n\r\n";
+		                	$notify_message .= sprintf( __('Website : %1$s (IP: %2$s , %3$s)', 'jabbernotification'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
+		                	$notify_message .= sprintf( __('URL    : %s', 'jabbernotification'), $comment->comment_author_url ) . "\r\n";
+		                	$notify_message .= __('Pingback excerpt: ', 'jabbernotification') . "\r\n" . $comment->comment_content . "\r\n\r\n";
 		                	break;
 		       
 				 default: //Comments
-		               		$notify_message  = sprintf( __('A new comment on the post #%1$s "%2$s" is waiting for your approval'), $post->ID, $post->post_title ) . "\r\n";
+		               		$notify_message  = sprintf( __('A new comment on the post #%1$s "%2$s" is waiting for your approval', 'jabbernotification'), $post->ID, $post->post_title ) . "\r\n";
 		                	$notify_message .= get_permalink($comment->comment_post_ID) . "\r\n\r\n";
-		                	$notify_message .= sprintf( __('Author : %1$s (IP: %2$s , %3$s)'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
-		                	$notify_message .= sprintf( __('E-mail : %s'), $comment->comment_author_email ) . "\r\n";
-		                	$notify_message .= sprintf( __('URL    : %s'), $comment->comment_author_url ) . "\r\n";
-		                	$notify_message .= sprintf( __('Whois  : http://ws.arin.net/cgi-bin/whois.pl?queryinput=%s'), $comment->comment_author_IP ) . "\r\n";
-		                	$notify_message .= __('Comment: ') . "\r\n" . $comment->comment_content . "\r\n\r\n";
+		                	$notify_message .= sprintf( __('Author : %1$s (IP: %2$s , %3$s)', 'jabbernotification'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
+		                	$notify_message .= sprintf( __('E-mail : %s', 'jabbernotification'), $comment->comment_author_email ) . "\r\n";
+		                	$notify_message .= sprintf( __('URL    : %s', 'jabbernotification'), $comment->comment_author_url ) . "\r\n";
+		                	$notify_message .= sprintf( __('Whois  : http://ws.arin.net/cgi-bin/whois.pl?queryinput=%s', 'jabbernotification'), $comment->comment_author_IP ) . "\r\n";
+		                	$notify_message .= __('Comment: ', 'jabbernotification') . "\r\n" . $comment->comment_content . "\r\n\r\n";
 		                	break;
 			}
 
-			$notify_message .= sprintf( __('Approve it: %s'),  admin_url("comment.php?action=approve&c=$comment_id") ) . "\r\n";
+			$notify_message .= sprintf( __('Approve it: %s', 'jabbernotification'),  admin_url("comment.php?action=approve&c=$comment_id") ) . "\r\n";
 			if ( EMPTY_TRASH_DAYS )
-		        	$notify_message .= sprintf( __('Trash it: %s'), admin_url("comment.php?action=trash&c=$comment_id") ) . "\r\n";
+		        	$notify_message .= sprintf( __('Trash it: %s', 'jabbernotification'), admin_url("comment.php?action=trash&c=$comment_id") ) . "\r\n";
 			else
-		        	$notify_message .= sprintf( __('Delete it: %s'), admin_url("comment.php?action=delete&c=$comment_id") ) . "\r\n";
+		        	$notify_message .= sprintf( __('Delete it: %s', 'jabbernotification'), admin_url("comment.php?action=delete&c=$comment_id") ) . "\r\n";
 			
-			$notify_message .= sprintf( __('Spam it: %s'), admin_url("comment.php?action=spam&c=$comment_id") ) . "\r\n";
+			$notify_message .= sprintf( __('Spam it: %s', 'jabbernotification'), admin_url("comment.php?action=spam&c=$comment_id") ) . "\r\n";
 			$notify_message .= sprintf( _n('Currently %s comment is waiting for approval. Please visit the moderation panel:',
 			        'Currently %s comments are waiting for approval. Please visit the moderation panel:', $comments_waiting), number_format_i18n($comments_waiting) ) . "\r\n";
 			$notify_message .= admin_url("edit-comments.php?comment_status=moderated") . "\r\n";
